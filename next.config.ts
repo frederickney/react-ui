e25f2accfeb2d72fda97adc850866f47d322b843
frederick <frederick.ney@gmail.com>
2025-06-02 14:08:55 +0200

    removing oauth2

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-import type { OIDC_CLIENT_SECRETS, OIDC_REALM} from "./oauth2/keycloak";
+//import type { OIDC_CLIENT_SECRETS, OIDC_REALM} from "./oauth2/keycloak";
 
 const nextConfig: NextConfig = {
   /* config options here */
